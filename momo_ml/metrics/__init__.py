2506bf517ee36999a9c6e92251b1942f9da1a7f5
Justin <47422999+YHJYH@users.noreply.github.com>
2026-03-17 14:43:31 +0000

    Update __init__.py

--- momo_ml/metrics/__init__.py
+++ momo_ml/metrics/__init__.py
@@ -2,10 +2,12 @@ from .psi import compute_psi
 from .ks import compute_ks
 from .kl import compute_kl
 from .js import compute_js
+from .wd import compute_wd
 
 __all__ = [
     "compute_psi",
     "compute_ks",
     "compute_kl",
     "compute_js",
+    "compute_wd",
 ]
